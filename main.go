23e1da5a9d4658138727cf1dc8a5b42a087c074c
Markus Blaschke <mblaschke82@gmail.com>
2017-09-30 18:37:38 +0200

    Improve handling
    - Add more configuration options
    - Add text lines for images
    - Add fallback stub

--- main.go
+++ main.go
@@ -9,15 +9,26 @@ import (
 	"image/png"
 	"image/gif"
 	"image/jpeg"
+	"image/color"
+	"image/draw"
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/inconsolata"
+	"golang.org/x/image/math/fixed"
+	"golang.org/x/image/font/basicfont"
 )
 
 type stubGenerator struct {
 	TemplateVariables map[string]string
 	Image struct {
+		BackgroundColor color.RGBA
+		FontColor color.RGBA
+		Font *basicfont.Face
+		FontLineHeight int
 		NumColors int
 		Quality int
 		Width int
 		Height int
+		Text []string
 	}
 	Overwrite bool
 }
@@ -25,10 +36,17 @@ type stubGenerator struct {
 func StubGenerator() stubGenerator {
 	stubGen := stubGenerator{}
 	stubGen.TemplateVariables = map[string]string{}
-	stubGen.Image.NumColors = 16
-	stubGen.Image.Quality = 70
-	stubGen.Image.Width = 100
-	stubGen.Image.Height = 100
+	stubGen.Image.BackgroundColor = color.RGBA{255, 255, 170, 255}
+	stubGen.Image.FontColor = color.RGBA{0, 0, 0, 255}
+	stubGen.Image.Font = inconsolata.Regular8x16
+	stubGen.Image.FontLineHeight = 15
+	stubGen.Image.NumColors = 256
+	stubGen.Image.Quality = 80
+	stubGen.Image.Width = 600
+	stubGen.Image.Height = 200
+	stubGen.Image.Text = []string{
+		"Path: %PATH%",
+	}
 	stubGen.Overwrite = false
 	return stubGen
 }
@@ -59,6 +77,8 @@ func (config stubGenerator) GenerateStub(path string) {
 	if config.generateStubText(path) {
 		return
 	}
+
+	config.generateStubFallback(path)
 }
 
 func (config stubGenerator) generateStubAuto(path string) bool {
@@ -66,10 +86,9 @@ func (config stubGenerator) generateStubAuto(path string) bool {
 
 	data, err := Asset(assetFile)
 	if err == nil {
-		f := createFile(path)
-		defer f.Close()
-		f.Write(data)
-		f.Sync()
+		createFile(path, func(f *os.File) {
+			f.Write(data)
+		})
 		return true
 	}
 
@@ -82,21 +101,15 @@ func (config stubGenerator) generateStubImage(path string) bool {
 	fileExt := filepath.Ext(path)
 	switch fileExt {
 	case ".gif":
-		options := gif.Options{
-			NumColors: config.Image.NumColors,
-		}
-
-		f := createFile(path)
-		defer f.Close()
-		img := image.NewRGBA(image.Rect(0, 0, config.Image.Width, config.Image.Height))
-		gif.Encode(f, img, &options)
+		createFile(path, func(f *os.File) {
+			gif.Encode(f, config.createImage(), &gif.Options{config.Image.NumColors, nil, nil})
+		})
 		return true
 
 	case ".png":
-		f := createFile(path)
-		defer f.Close()
-		img := image.NewRGBA(image.Rect(0, 0, config.Image.Width, config.Image.Height))
-		png.Encode(f, img)
+		createFile(path, func(f *os.File) {
+			png.Encode(f, config.createImage())
+		})
 		return true
 
 	case ".jpg":
@@ -106,14 +119,32 @@ func (config stubGenerator) generateStubImage(path string) bool {
 			Quality: config.Image.Quality,
 		}
 
-		f := createFile(path)
-		defer f.Close()
-		img := image.NewRGBA(image.Rect(0, 0, config.Image.Width, config.Image.Height))
-		jpeg.Encode(f, img, &options)
+		createFile(path, func(f *os.File) {
+			jpeg.Encode(f, config.createImage(), &options)
+		})
+
 		return true
 	}
 
 	return false
+}
+
+func (config stubGenerator) createImage() *image.RGBA {
+	img := image.NewRGBA(image.Rect(0, 0, config.Image.Width, config.Image.Height))
+	draw.Draw(img, img.Bounds(), &image.Uniform{config.Image.BackgroundColor}, image.ZP, draw.Src)
+
+	for num, line := range config.Image.Text {
+		for key, value := range config.TemplateVariables {
+			replaceKey := fmt.Sprintf("%%%s%%", key)
+			line = strings.Replace(line, replaceKey, value, -1)
+		}
+
+		posX := 10
+		posY := 10 + (config.Image.FontLineHeight * (num + 1))
+		config.imageWriteTextLine(img, posX, posY, line)
+	}
+
+	return img
 }
 
 func (config stubGenerator) generateStubText(path string) bool {
@@ -128,10 +159,9 @@ func (config stubGenerator) generateStubText(path string) bool {
 			content = strings.Replace(content, replaceKey, value, -1)
 		}
 
-		f := createFile(path)
-		defer f.Close()
-		f.WriteString(content)
-		f.Sync()
+		createFile(path, func(f *os.File) {
+			f.WriteString(content)
+		})
 
 		return true
 	}
@@ -139,12 +169,29 @@ func (config stubGenerator) generateStubText(path string) bool {
 	return false
 }
 
-func createFile(path string) *os.File {
+func (config stubGenerator) generateStubFallback(path string) bool {
+	createFile(path, func(f *os.File) {
+		for key, value := range config.TemplateVariables {
+			f.WriteString(fmt.Sprintf("%s: %s", key, value))
+		}
+	})
+
+	return true
+}
+
+func createFile(path string, callback func(f *os.File)) *os.File {
 	directory := filepath.Dir(path)
 	os.MkdirAll(directory, os.ModePerm)
 	
 	f, err := os.Create(path)
 	check(err)
+	defer func() {
+		f.Close()
+		f.Sync()
+	}()
+
+	callback(f)
+
 	return f
 }
 
@@ -152,4 +199,16 @@ func check(e error) {
 	if e != nil {
 		panic(e)
 	}
+}
+
+func (config stubGenerator) imageWriteTextLine(img *image.RGBA, x, y int, label string) {
+	point := fixed.Point26_6{fixed.Int26_6(x * 64), fixed.Int26_6(y * 64)}
+
+	d := &font.Drawer{
+		Dst:  img,
+		Src:  image.NewUniform(config.Image.FontColor),
+		Face: config.Image.Font,
+		Dot:  point,
+	}
+	d.DrawString(label)
 }
